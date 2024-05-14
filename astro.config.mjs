8a6bd12f993b2e57be5cbeb338e67368b53a0d91
Stephanie Hays <steph@stephhays.dev>
2024-05-14 17:27:55 -0500

    fixup astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
+import remarkCustomHeaderId from 'remark-custom-header-id';
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        syntaxHighlight: false, // Temporarily disable syntax highlighting and rely on Prism.js via choco-theme
+        remarkPlugins: [
+            remarkCustomHeaderId,
+        ],
+    },
     integrations: [sitemap(), mdx()]
 });
